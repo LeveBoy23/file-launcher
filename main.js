b9e8d077837c52b9ad6319828aedaac9b8826bc0
Levi McClements <114172881+LeveBoy23@users.noreply.github.com>
2025-11-15 22:24:16 -0800

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('load', async()=>{
-if(!(await testConnectionSupabase())) return;
-const games=await getGamesCached(); renderGameList(games);
-document.getElementById('search-input').oninput=()=>{ const t=search.value.toLowerCase(); renderGameList(allGames.filter(g=>g.Name.toLowerCase().includes(t))); };
-});
